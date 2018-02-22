6e96f358024f4436d3d49faf72d659f7cb119fa9
vegito165 <15130052@st.hcmuaf.edu.vn>
2018-02-22 10:33:13 +0700

    klm

--- public/database/LT.js
+++ public/database/LT.js
@@ -2,5 +2,9 @@ var listLT = [
     {
         id: 'C1_DaiSoMenhDe',
         name: 'Chương 1 - Đại số mệnh đề'
+    },
+    {
+        id: 'C2_HeToanMenhDe',
+        name: 'Chương 2 - Hệ toán mệnh đề'
     }
 ];
